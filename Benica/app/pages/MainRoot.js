eddeb9ea71075feff901e73e3c855ec47270ab1d
BigdaddyTedy <0130526@gmail.com>
2023-03-01 22:26:19 +0800

    Bottomtab icons

--- Benica/app/pages/MainRoot.js
+++ Benica/app/pages/MainRoot.js
@@ -1,16 +1,31 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet, SafeAreaView } from "react-native";
 import Constants from "expo-constants";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import Home from "./Home.Js";
+import Favorite from "./Favorite";
+import Settings from "./Settings";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+const Tab = createMaterialBottomTabNavigator();
+
 class MainRoot extends Component {
   render() {
     return (
-      <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="Home" component={Home} />
-          <Tab.Screen name="Favorite" component={Favorite} />
-          <Tab.Screen name="Settings" component={Settings} />
-        </Tab.Navigator>
-      </NavigationContainer>
+      <Tab.Navigator initialRouteName="Home">
+        <Tab.Screen
+          options={{
+            tabBarLabel: "Home",
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="home" color={color} size={26} />
+            ),
+          }}
+          name="Home"
+          component={Home}
+        />
+        <Tab.Screen name="Favorite" component={Favorite} />
+        <Tab.Screen name="Settings" component={Settings} />
+      </Tab.Navigator>
     );
   }
 }

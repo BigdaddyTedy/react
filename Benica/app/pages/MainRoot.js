56c508ce84545045a78feaa621b1ddc099402ad9
BigdaddyTedy <112466128+BigdaddyTedy@users.noreply.github.com>
2023-03-13 10:07:21 +0800

    asd

--- Benica/app/pages/MainRoot.js
+++ Benica/app/pages/MainRoot.js
@@ -1,53 +1,147 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  ActivityIndicator,
+  SafeAreaView,
+  Alert,
+} from "react-native";
 import Constants from "expo-constants";
-import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
-import Home from "./Home";
-import Favorite from "./Favorite";
-import Settings from "./Settings";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Home from "./../pages/Home";
+import Favorite from "./../pages/Favorite";
+import Settings from "./../pages/Settings";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
-
-const Tab = createMaterialBottomTabNavigator();
-
+import * as SQLite from "expo-sqlite";
+import { ThemeContext } from "../contexts/ThemeContext";
+import { IMLocalized } from "../IMLocalized";
+const Tab = createBottomTabNavigator();
+const db = SQLite.openDatabase("movie.db");
 class MainRoot extends Component {
+  constructor(props) {
+    super(props);
+
+    //TODO: Disable go back [https://reactnavigation.org/docs/preventing-going-back/]
+    props.navigation.addListener("beforeRemove", (e) => {
+      e.preventDefault();
+      return;
+    });
+
+    this.state = {
+      isLoading: true,
+      genres: [],
+    };
+
+    db.transaction((tx) => {
+      tx.executeSql(
+        "CREATE TABLE IF NOT EXISTS Favorites (id INTEGER PRIMARY KEY AUTOINCREMENT, movie_id INT, title TEXT, genres TEXT, overview TEXT, popularity TEXT, release_date TEXT, vote_average TEXT, vote_count TEXT, poster TEXT, backdrop TEXT);"
+      );
+    });
+
+    this.fetchData();
+  }
+
+  fetchData() {
+    return fetch(
+      "https://api.themoviedb.org/3/genre/movie/list?api_key=802b2c4b88ea1183e50e6b285a27696e"
+    )
+      .then((response) => response.json())
+      .then((responseJson) => {
+        //TODO: Localize for Genres
+        responseJson.genres.map((genre) => {
+          const key = genre.name.toString().toLowerCase().replace(" ", "");
+          genre.name = IMLocalized(key);
+        });
+
+        this.setState({
+          isLoading: false,
+          genres: responseJson.genres,
+        });
+      })
+      .catch((error) => console.error(error));
+  }
+
   render() {
-    return (
-      <Tab.Navigator
-        activeColor="#e91e63"
-        barStyle={{ backgroundColor: "tomato" }}
-        initialRouteName="Home"
+    const HomeComponent = (props) => (
+      <Home {...props} genres={this.state.genres} />
+    );
+    if (this.state.isLoading) {
+      <SafeAreaView
+        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
       >
-        <Tab.Screen
-          options={{
-            tabBarLabel: "Home",
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name="home" color={color} size={26} />
-            ),
-          }}
-          name="Home"
-          component={Home}
-        />
-        <Tab.Screen
-          options={{
-            tabBarLabel: "Favorite",
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name="heart" color={color} size={26} />
-            ),
-          }}
-          name="Favorite"
-          component={Favorite}
-        />
-        <Tab.Screen
-          options={{
-            tabBarLabel: "Home",
-            tabBarIcon: ({ color }) => (
-              <MaterialCommunityIcons name="cog" color={color} size={26} />
-            ),
-          }}
-          name="Settings"
-          component={Settings}
-        />
-      </Tab.Navigator>
+        <ActivityIndicator />
+      </SafeAreaView>;
+    }
+    return (
+      <ThemeContext.Consumer>
+        {(context) => {
+          const { isDarkMode, light, dark } = context;
+          return (
+            <Tab.Navigator
+              tabBarOptions={{
+                activeTintColor: isDarkMode ? "#FFF" : "#333",
+                inactiveTintColor: "#999",
+                keyboardHidesTabBar: true,
+                labelStyle: { fontFamily: "poppins-r" },
+                style: {
+                  backgroundColor: isDarkMode ? dark.bg : light.bg,
+                  borderTopWidth: 0,
+                  shadowColor: "#000",
+                  shadowOffset: {
+                    width: 0,
+                    height: 4,
+                  },
+                  shadowOpacity: 0.25,
+                  shadowRadius: 8,
+                },
+              }}
+              initialRouteName="Home"
+            >
+              <Tab.Screen
+                options={{
+                  tabBarLabel: IMLocalized("home"),
+                  tabBarIcon: ({ color, size }) => (
+                    <MaterialCommunityIcons
+                      name="home"
+                      color={color}
+                      size={22}
+                    />
+                  ),
+                }}
+                name={IMLocalized("home")}
+                component={HomeComponent}
+              />
+              <Tab.Screen
+                options={{
+                  tabBarLabel: IMLocalized("favorites"),
+                  tabBarIcon: ({ color, size }) => (
+                    <MaterialCommunityIcons
+                      name="heart"
+                      color={color}
+                      size={22}
+                    />
+                  ),
+                }}
+                name={IMLocalized("favorites")}
+                component={Favorite}
+              />
+              <Tab.Screen
+                options={{
+                  tabBarLabel: IMLocalized("settings"),
+                  tabBarIcon: ({ color, size }) => (
+                    <MaterialCommunityIcons
+                      name="cog"
+                      color={color}
+                      size={22}
+                    />
+                  ),
+                }}
+                name={IMLocalized("settings")}
+                component={Settings}
+              />
+            </Tab.Navigator>
+          );
+        }}
+      </ThemeContext.Consumer>
     );
   }
 }
@@ -56,7 +150,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: Constants.statusBarHeight,
-    m,
   },
 });
+
 export default MainRoot;
